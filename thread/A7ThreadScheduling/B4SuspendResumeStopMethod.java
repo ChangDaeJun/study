4475452568929ed93445937a343d3ed78acc6c6b
ChangDaeJun <eowns1209@gmail.com>
2023-01-10 11:38:03 +0900

    thread scheduling 2

---
+++ thread/A7ThreadScheduling/B4SuspendResumeStopMethod.java
@@ -0,0 +1,10 @@
+package thread.A7ThreadScheduling;
+
+/**
+ *  * [suspend()] & [resume()] & [stop()] - deprecated! 된 함수(데드락 발생 가능성을 높임)
+ *  * 1. suspend(): 쓰레드를 멈춤.
+ *  * 2. resume(): 멈춘 쓰레드를 실행.
+ *  * 3. stop(): 쓰레드 즉시 종료.
+ */
+public class B4SuspendResumeStopMethod {
+}
