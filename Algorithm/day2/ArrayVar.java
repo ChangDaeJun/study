1deab3b7f474b0fe3e675825c8b4ec0b32ff315e
ChangDaeJun <eowns1209@gmail.com>
2023-01-17 10:39:24 +0900

    2차원 배열 프로그램 예제

---
+++ Algorithm/day2/ArrayVar.java
@@ -0,0 +1,11 @@
+package Algorithm.day2;
+
+public class ArrayVar {
+
+    int[] arr;
+
+    public static void main(String[] args) {
+        ArrayVar arrayVar = new ArrayVar();
+        System.out.println(arrayVar.arr);
+    }
+}
--- Algorithm/day2/StaticVar.java
+++ Algorithm/day2/StaticVar.java
@@ -2,10 +2,13 @@ package Algorithm.day2;
 
 public class StaticVar {
 
-    public static int num;
+    public static int num = 3;
 
 
     public static void main(String[] args) {
+        System.out.println(num);
+        StaticVar staticVar = new StaticVar();
+        staticVar.num *= 3;
         System.out.println(num);
     }
 }
